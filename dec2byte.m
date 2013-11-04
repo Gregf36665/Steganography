bd0af9f5e48e68e8d8a2a382d983291f03cc9638
Gregf36665 <Flynng@lafayette.edu>
2013-11-03 20:50:45 -0500

    Create dec2byte.m
    A basic function to convert a number into an 8 bit stream

---
+++ dec2byte.m
@@ -0,0 +1,10 @@
+function [byte] = dec2byte(decimal)
+%dec2byte Convert decimal to a 8 bit stream
+%   Detailed explanation goes here
+byte = zeros(1,8);
+while decimal ~= 0,
+    x = floor(log2(decimal));
+    byte(8-x) = 1;
+    decimal = decimal - pow2(x);
+end
+
