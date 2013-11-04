ea3d07aec21feea4890e2a4504735b5fb5b2033c
Gregf36665 <Flynng@lafayette.edu>
2013-11-03 20:51:43 -0500

    Update dec2byte.m

--- dec2byte.m
+++ dec2byte.m
@@ -2,6 +2,7 @@ function [byte] = dec2byte(decimal)
 %dec2byte Convert decimal to a 8 bit stream
 %   Detailed explanation goes here
 byte = zeros(1,8);
+decimal = rem(decimal,256);
 while decimal ~= 0,
     x = floor(log2(decimal));
     byte(8-x) = 1;
