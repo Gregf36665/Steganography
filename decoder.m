73b24f07dad93e5c883dafced800708853126d2a
Gregf36665 <Flynng@lafayette.edu>
2013-11-04 22:44:23 -0500

    Update decoder.m
    This code should be able to deal with decoding an image.  There was an error when it ran; the output colour was all over the place but this is believed to be due to the encryption not being completed as red was correct throughout.

--- decoder.m
+++ decoder.m
@@ -19,19 +19,15 @@ coded_b = coded(:,:,3);
 
 %convert them to binary:
 
-coded_bin_r = dec2bin(coded_r,8);
-coded_bin_g = dec2bin(coded_g,8);
-coded_bin_b = dec2bin(coded_b,8);
-
-coded_bit_r = coded_bin_r(:,8); %this is the 8th bit
-coded_bit_g = coded_bin_g(:,8);
-coded_bit_b = coded_bin_b(:,8);
+coded_bin_r = rem(coded_r,2);
+coded_bin_g = rem(coded_g,2);
+coded_bin_b = rem(coded_b,2);
 
 % Find the dimensions for the hidden image
 height_hidden_bin = '0000000000000000';
 
 for i=1:1:16,
-    height_hidden_bin(i) = num2str(coded_bit_r(i));
+    height_hidden_bin(i) = num2str(coded_bin_r(i));
 end
 
 height_hidden = bin2dec(height_hidden_bin);
@@ -39,12 +35,16 @@ height_hidden = bin2dec(height_hidden_bin);
 width_hidden_bin = '0000000000000000';
 
 for i=1:1:16,
-    width_hidden_bin(i) = num2str(coded_bit_g(i));
+    width_hidden_bin(i) = num2str(coded_bin_g(i));
 end
+
+width_hidden = bin2dec(width_hidden_bin);
 
 clear i;
 
-width_hidden = bin2dec(width_hidden_bin);
+% End of the dimension calculations
+
+hidden_dimensions = height_hidden * width_hidden;
 
 hidden_r = zeros(height_hidden,width_hidden);
 hidden_g = zeros(height_hidden,width_hidden);
@@ -56,18 +56,19 @@ hidden_b = zeros(height_hidden,width_hidden);
 bits = width_hidden * height_hidden * 8;
 
 
-for i=2:1:bits/8,
-    hidden_r(i) = bin2dec(num2str(str2double(coded_bit_r(8*i+1:8*i+8))));
+for i=1:1:hidden_dimensions,
+    hidden_r(i) = bin2dec(num2str(coded_bin_r(8*i+9:8*i+16)));
 end
 
-for i=1:1:bits/8,
-    hidden_g(i) = bin2dec(num2str(str2double(coded_bit_g(8*i+9:8*i+16))));
+for i=1:1:hidden_dimensions,
+    hidden_g(i) = bin2dec(num2str(coded_bin_g(8*i+9:8*i+16)));
 end
 
-for i=1:1:bits/8,
-    hidden_b(i) = bin2dec(num2str(str2double(coded_bit_b(8*i+9:8*i+16))));
+for i=1:1:hidden_dimensions,
+    hidden_b(i) = bin2dec(num2str(coded_bin_b(8*i+9:8*i+16)));
 end
 
-hidden(:,:,1) = hidden_r;
-hidden(:,:,2) = hidden_g;
-hidden(:,:,3) = hidden_b;
+hidden(:,:,1) = uint8(hidden_r);
+hidden(:,:,2) = uint8(hidden_g);
+hidden(:,:,3) = uint8(hidden_b);
+
