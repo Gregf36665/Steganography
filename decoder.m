9b2ff7a52a2f4ededa6ba2f51e0034bdea7f6c6a
Gregf36665 <gregory@mmflynn.com>
2013-11-21 20:49:33 -0500

    Update basic.html
    I adjusted basic.html so that it looked nicer.  I also encluded my
    writeup on the entire project and the older code.
    I then performed a massive tidy up so that only the code that is valid
    for this project

--- decoder.m
+++ decoder.m
@@ -1,28 +1,32 @@
+%% Read
+% Ask for the coded file
 clear all 
-
-% This is the decoder
-
 coded = imread(input('Coded file: ','s'));
 
 dim_coded = size(coded);
 
-% Calcualte the height and width of the coded image:
-
+%% Calcualte 
+% Find the height and width of the coded image
 coded_height = dim_coded(1);
 coded_width = dim_coded(2);
 coded_size = coded_height * coded_width;
-% Find the red green and blue channels of the image:
+
+%% Coded colors
+% Find the RGB values
 
 coded_r = coded(:,:,1); 
 coded_g = coded(:,:,2);
 coded_b = coded(:,:,3);
 
-%convert them to binary:
+%% Coded binary
+% Find the binary values of the RGB found previously
 
 coded_bin_r = rem(coded_r,2);
 coded_bin_g = rem(coded_g,2);
 coded_bin_b = rem(coded_b,2);
+disp('Binary values found');
 
+%% Dimension
 % Find the dimensions for the hidden image
 height_hidden_bin = '0000000000000000';
 
@@ -41,8 +45,10 @@ end
 width_hidden = bin2dec(width_hidden_bin);
 
 clear i;
-
+disp('Dimensions found');
 % End of the dimension calculations
+%% Size hidden image
+% Make an array for the hidden image
 
 hidden_dimensions = height_hidden * width_hidden;
 
@@ -50,25 +56,33 @@ hidden_r = zeros(height_hidden,width_hidden);
 hidden_g = zeros(height_hidden,width_hidden);
 hidden_b = zeros(height_hidden,width_hidden);
 
-% find the values of r
+%% Hidden RGB
+% Find the hidden RGB values in binary and convert them to double
+
 % bits required:
 
-bits = width_hidden * height_hidden * 8;
+bits = hidden_dimensions * 8;
 
-
+% red
 for i=1:1:hidden_dimensions,
     hidden_r(i) = bin2dec(num2str(coded_bin_r(8*i+9:8*i+16)));
 end
+disp('Red found');
 
+% green
 for i=1:1:hidden_dimensions,
     hidden_g(i) = bin2dec(num2str(coded_bin_g(8*i+9:8*i+16)));
 end
+disp('Green found');
 
+% blue
 for i=1:1:hidden_dimensions,
     hidden_b(i) = bin2dec(num2str(coded_bin_b(8*i+9:8*i+16)));
 end
+disp('Blue found');
 
+%% Merge RGB
+% Convert the doubles to uint8
 hidden(:,:,1) = uint8(hidden_r);
 hidden(:,:,2) = uint8(hidden_g);
 hidden(:,:,3) = uint8(hidden_b);
-
--- final/basic.m
+++ final/basic.m
@@ -1,4 +1,5 @@
-% Set up:
+%% Set up
+% This reads all of the data in and makes it into the right format
 clear all
 noise=audioread('cookiemonster.wav');
 figure('name','Audio');
@@ -15,35 +16,35 @@ figure(3)
 image(hidden)
 cover=double(cover);
 edit=cover;
-%%
-% Hide the picture
+%% Hide the picture
+% this is basic addition on the first 256x256px
 edit(1:256,1:256,:)=cover(1:256,1:256,:)+double(hidden)/100;
 figure('name','Cover with image hidden');
 figure(4)
 image(edit/256)
-%%
-% Hide the sound
+%% Hide the sound
+% This is basic addition on the next 64x1103px
 noise=noise(1:70592,1); % Ensure the dimensions are correct
 
 % Reshape to fit into the image
 edit(257:320,1:1103,1)=(cover(257:320,1:1103,1))+reshape(noise,64,1103)*20+1; 
 
-%%
+%% Show hidden
 % Display the cover image with the hidden image and audio
 figure('name','Cover with everythin hidden');
 figure(5)
 edit_uint = uint8(edit);
 image(edit_uint)
 
-%%
-% Decode the image
+%% Decode pic
+% Extract the hidden image
 figure('name','Extracted hidden image');
 figure(6)
 hidden_out = (edit(1:256,1:256,:)-(cover(1:256,1:256,:)))*100 ;
 image(uint8(hidden_out))
 
-%%
-% Decode the audio
+%% Decode sound
+% Extract the hidden audio
 figure('name','Extracted audio');
 audio_out=reshape(edit(257:320,1:1103,1)-cover(257:320,1:1103,1),70592,1);
 figure(7)
